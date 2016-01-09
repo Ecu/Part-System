1bfcc45615adc0da830eb33a57d7f2a4d3d4fe67
Ecu <ecu.truin@gmail.com>
2016-01-09 11:41:29 -0600

    Update to ignore and adding code.

---
+++ Editor/PartInspector.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Part))]
+public class PartInspector : Editor
+{
+	SerializedProperty _collection;
+	SerializedProperty _frame;
+	private Part part;
+	GameObject collection = null;
+
+	void OnEnable ()
+	{
+		_collection = serializedObject.FindProperty ("_collection");
+		_frame = serializedObject.FindProperty ("_frame");
+
+		part = (Part)target;
+		MeshFilter meshFilter;
+		MeshRenderer meshRenderer;
+
+		meshFilter = part.gameObject.GetComponent<MeshFilter> ();
+		if (meshFilter == null) {
+			meshFilter = part.gameObject.AddComponent<MeshFilter> ();
+		}			
+		meshFilter.hideFlags = HideFlags.HideInInspector;
+
+		meshRenderer = part.gameObject.GetComponent<MeshRenderer> ();
+		if (meshRenderer == null) {
+			meshRenderer = part.gameObject.AddComponent<MeshRenderer> ();
+		}			
+		meshRenderer.hideFlags = HideFlags.HideInInspector;
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		serializedObject.Update ();
+
+		// Show field to select collection.
+		EditorGUILayout.PropertyField (_collection);
+
+		// Get game object stored in serialized property.
+		collection = (GameObject)_collection.objectReferenceValue;
+
+		bool errored = false;
+
+		if (collection == null) {
+			EditorGUILayout.HelpBox ("Please select a collection to use with this part.", MessageType.Info);
+			errored = true;
+		}
+		// Empty collection found.  Display error warning.
+		else if (collection.transform.childCount < 1) {
+			EditorGUILayout.HelpBox ("Invalid collection.  Please choose another collection.", MessageType.Error);
+			errored = true;
+		}
+
+		int index = Mathf.FloorToInt (_frame.floatValue);
+		if (!errored && index > collection.transform.childCount) {
+			index = 0;
+			_frame.floatValue = (float)index;
+		}
+		if (!errored) {
+			ArrayList options = new ArrayList ();
+			options.Add ("None");
+			foreach (Transform child in collection.transform) {
+				options.Add (child.name.Replace ("_", " "));
+			}
+			index = EditorGUILayout.Popup ("Part", index, (string[])options.ToArray (typeof(string)), EditorStyles.popup);
+		}
+
+		if (GUI.changed) {
+			if (collection == null) {
+				index = 0;
+			}
+			_frame.floatValue = (float)index;
+		}
+
+		serializedObject.ApplyModifiedProperties ();
+	}
+}
---
+++ Part.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+[ExecuteInEditMode]
+public class Part : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject
+		_collection = null;
+	[SerializeField]
+	private float
+		_frame = 0.0f;
+	private bool changed = false;
+
+	public string part {
+		get {
+			if (_collection == null || _frame == 0.0f) {
+				return "None";
+			} else {
+				return _collection.transform.GetChild (Mathf.FloorToInt (_frame) - 1).gameObject.name;
+			}
+		}
+		set {
+			value = value.Replace (" ", "_");
+			if (_collection == null || value == "None") {
+				frame = 0;
+			} else {
+				int index = _collection.transform.FindChild (value).GetSiblingIndex ();
+				if (index != -1) {
+					frame = index + 1;
+				} else {
+					frame = 0;
+				}
+			}
+		}
+	}
+
+	public int frame {
+		get {
+			return Mathf.FloorToInt (_frame);
+		}
+		set {
+			_frame = (float)value;
+			changed = true;
+		}
+	}
+
+	void OnValidate ()
+	{
+		changed = true;
+	}
+
+	void OnDidApplyAnimationProperties ()
+	{
+		changed = true;
+	}
+
+	void Update ()
+	{
+		if (changed) {
+
+			changed = false;
+
+			MeshFilter meshFilter = GetComponent<MeshFilter> ();
+			MeshRenderer meshRenderer = GetComponent<MeshRenderer> ();
+
+			if (_collection != null && _frame != 0.0f) {
+				GameObject found = _collection.transform.GetChild (Mathf.FloorToInt (_frame) - 1).gameObject;
+				meshFilter.mesh = found.GetComponent<MeshFilter> ().sharedMesh;
+				meshRenderer.material = found.GetComponent<MeshRenderer> ().sharedMaterial;
+			} else {
+				meshFilter.mesh = null;
+				meshRenderer.material = null;
+			}
+		}
+	}
+}
